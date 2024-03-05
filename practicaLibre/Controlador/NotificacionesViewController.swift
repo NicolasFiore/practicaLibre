7a403454c77ab22e8b274a33e4acf0c8271fb9b6
Nicolás Fiore <nicolasfiore@Mac-mini-de-Nicolas.local>
2024-03-05 12:02:01 -0300

    forzado de cambio de color titulo deleteAction

--- practicaLibre/Controlador/NotificacionesViewController.swift
+++ practicaLibre/Controlador/NotificacionesViewController.swift
@@ -12,6 +12,7 @@ class NotificacionesViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var notificaciones: [NotificacionesModelo] = []
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,12 @@ class NotificacionesViewController: UIViewController {
         tableView.delegate = self
         tableView.register(UINib(nibName: "NotificacionTableViewCell", bundle: nil), forCellReuseIdentifier: "celdaNotificacion")
         
-        var notificacion1 = NotificacionesModelo(titulo: "¡Nicolás tu iphone 15 te está esperando!", imagen: UIImage.iphone15)
-        var notificacion2 = NotificacionesModelo(titulo: "¡Nicolás tus AirPods Pro 2 te está esperando!", imagen: UIImage.airpodsPro2)
+        let notificacion1 = NotificacionesModelo(titulo: "¡Nicolás tu iphone 15 te está esperando!", imagen: UIImage.iphone15)
+        let notificacion2 = NotificacionesModelo(titulo: "¡Nicolás tus AirPods Pro 2 te está esperando!", imagen: UIImage.airpodsPro2)
         notificaciones.append(notificacion1)
         notificaciones.append(notificacion2)
     }
+    
 
 }
 extension NotificacionesViewController: UITableViewDataSource {
@@ -45,15 +47,21 @@ extension NotificacionesViewController: UITableViewDelegate {
         return .delete
     }
     
+    //se cubren todos los casos en los que, por algún motivo, no se llamaron en funciones anteriores.
+    func tableView(_ tableView: UITableView, willBeginEditingRowAt indexPath: IndexPath) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+            tableView.cellForRow(at: indexPath)?.layoutIfNeeded()
+        }
+    }
+    
     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath)
             -> UISwipeActionsConfiguration? {
-            let deleteAction = UIContextualAction(style: .destructive, title: nil) { (_, _, completionHandler) in
+                let deleteAction = UIContextualAction(style: .destructive, title: nil) { (_, _, completionHandler) in
                 self.notificaciones.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .fade)
                 completionHandler(true)
             }
                 deleteAction.title = "Eliminar"
-                
                 deleteAction.backgroundColor = .barraBuscador
                 let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
                 return configuration
@@ -70,5 +78,26 @@ extension NotificacionesViewController: UITableViewDelegate {
                 deleteAction.backgroundColor = .barraBuscador
                 let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
                 return configuration
+    }
+}
+
+// forzar cambio de color de las etiquetas de acciones
+// https://stackoverflow.com/questions/26887563/how-to-change-uitableviewrowaction-title-color
+extension UITableViewCell {
+    var cellActionButtonLabel: UILabel? {
+        superview?.subviews
+            .filter { String(describing: $0).range(of: "UISwipeActionPullView") != nil }
+            .flatMap { $0.subviews }
+            .filter { String(describing: $0).range(of: "UISwipeActionStandardButton") != nil }
+            .flatMap { $0.subviews }
+            .compactMap { $0 as? UILabel }.first
+    }
+    open override func layoutSubviews() {
+        super.layoutSubviews()
+        cellActionButtonLabel?.textColor = .gray
+    }
+    open override func layoutIfNeeded() {
+        super.layoutIfNeeded()
+        cellActionButtonLabel?.textColor = .gray
     }
 }
